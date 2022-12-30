2ce52afa6b5d6cbcdcb135cb69789f7478f10cda
domCode <alessandro.pegoraro@studenti.univr.it>
2022-12-30 15:29:44 +0100

    Fixed blocking game loop with GetMessage, using PeekMessage now.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -2,6 +2,8 @@
 
 #include "Windows_Fixed.h"
 #include "Window.h"
+#include <chrono>
+#include <string>
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
@@ -10,24 +12,32 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	// Get messages //
 
-	MSG message;
-	BOOL get_result;
-	while ( (get_result = GetMessage(&message, nullptr, 0, 0)) > 0)
-	{
-		TranslateMessage(&message);
-		
-		DispatchMessage(&message);
+	auto prev_frame = std::chrono::steady_clock::now();
 
-		if (window.keyboard.isKeyPressed('F'))
+	MSG message;
+	bool running = true;
+	while (running)
+	{
+		while (PeekMessage(&message, nullptr, 0, 0, PM_REMOVE))
 		{
-			window.setWindowName("Paganni Gianni");
+			if (message.message == WM_QUIT)
+			{
+				running = false;
+			}
+
+			TranslateMessage(&message);
+			DispatchMessage(&message);
 		}
 
-		if (window.keyboard.isKeyPressed('G'))
+		auto elapsed_time = std::chrono::steady_clock::now() - prev_frame;
+		prev_frame = std::chrono::steady_clock::now();
+		long long seconds_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_time).count();
+
+		if (window.keyboard.isKeyPressed(' '))
 		{
-			window.setWindowName("Gianni Paganni");
+			MessageBox(nullptr, std::to_string(seconds_elapsed).c_str(), "Delta Time", MB_OK);
 		}
 	}
 
-	return get_result == -1 ? -1 : message.wParam;
+	return message.wParam;
 }
