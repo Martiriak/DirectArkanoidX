21ecb5b569c4c4077a34749deaf05b9902f9472c
domCode <alessandro.pegoraro@studenti.univr.it>
2022-12-30 23:04:00 +0100

    Added small background + delta_time test.

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -10,12 +10,44 @@ Application::Application(int window_width, int window_height, LPCSTR window_name
 
 void Application::processFrame(float delta_time)
 {
+	static constexpr float lerp_speed = 1.f;
+	static bool is_reversed = false;
+	static float elapsed_time = 0.f;
+	static float red = 0.f;
+	static float green = 1.f;
+	static constexpr float blue = 0.f;
+
 	if (window.keyboard.isKeyPressed(' '))
 	{
 		MessageBox(nullptr, std::to_string(delta_time).c_str(), "Delta Time", MB_OK);
 	}
 
-	window.getRenderer().clearBuffer(0.2f, 0.6f, 0.2f);
+	if (!is_reversed)
+	{
+		if (elapsed_time > 1.f)
+		{
+			is_reversed = !is_reversed;
+			elapsed_time = 1.f;
+		}
+	}
+	else
+	{
+		if (elapsed_time < 0.f)
+		{
+			is_reversed = !is_reversed;
+			elapsed_time = 0.f;
+		}
+	}
+
+	red = elapsed_time;
+	green = 1.f - elapsed_time;
+
+	float increment = is_reversed ? -delta_time : delta_time;
+	increment *= lerp_speed;
+
+	elapsed_time += increment;
+
+	window.getRenderer().clearBuffer(red, green, blue);
 
 	window.getRenderer().present();
 }
