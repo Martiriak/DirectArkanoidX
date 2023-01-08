196955eb0477ceb83e4234a5e7730c06447c468a
domCode <alessandro.pegoraro@studenti.univr.it>
2023-01-08 23:33:43 +0100

    Added Vertex and Color structs.

---
+++ Headers/Color.h
@@ -0,0 +1,11 @@
+// Alessandro Pegoraro - Graphics Programming
+
+#pragma once
+
+
+struct Color
+{
+	using byte = unsigned char;
+
+	byte r, g, b, a;
+};
---
+++ Headers/Vertex.h
@@ -0,0 +1,13 @@
+// Alessandro Pegoraro - Graphics Programming
+
+#pragma once
+
+#include "Color.h"
+
+
+struct Vertex
+{
+	struct { float x, y; } position;
+
+	Color color;
+};
