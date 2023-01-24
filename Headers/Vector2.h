fc1ad8aadb0faaa4b7a5f2c4b224ee1c370d811e
domCode <alessandro.pegoraro@studenti.univr.it>
2023-01-24 19:07:08 +0100

    Improved Vector2 and fixed dot product.

--- Headers/Vector2.h
+++ Headers/Vector2.h
@@ -8,8 +8,8 @@ struct Vector2 final
 	float x, y;
 
 
-	inline Vector2() : Vector2(0.f) { }
-	inline Vector2(float uniform) : x(uniform), y(uniform) { }
+	inline explicit Vector2() : Vector2(0.f) { }
+	inline explicit Vector2(float uniform) : x(uniform), y(uniform) { }
 	inline Vector2(float _x, float _y) : x(_x), y(_y) { }
 
 
@@ -25,12 +25,19 @@ struct Vector2 final
 	inline Vector2 operator- () const { return Vector2(-x, -y); }
 
 
+	Vector2 getNormalized() const;
+	inline Vector2& normalize() { return *this = getNormalized(); }
+
+	float getLenght() const;
+	inline float getSquaredLenght() const { return dot(*this, *this); }
+
+
 	static inline Vector2 zero() { return Vector2(0.f); }
 	static inline Vector2 one() { return Vector2(1.f); }
 	static inline Vector2 right() { return Vector2(1.f, 0.f); }
 	static inline Vector2 up() { return Vector2(0.f, 1.f); }
 
-	static Vector2 dot(const Vector2& right_side, const Vector2& left_side);
+	static float dot(const Vector2& right_side, const Vector2& left_side);
 };
 
 
@@ -77,7 +84,7 @@ inline Vector2 operator/ (const Vector2& vector, float scalar)
 
 
 
-inline Vector2 operator* (const Vector2& right_side, const Vector2& left_side)
+inline float operator* (const Vector2& right_side, const Vector2& left_side)
 {
 	return Vector2::dot(right_side, left_side);
 }
--- Source/Vector2.cpp
+++ Source/Vector2.cpp
@@ -1,6 +1,7 @@
 // Alessandro Pegoraro - Graphics Programming
 
 #include "Vector2.h"
+#include <cmath>
 
 
 Vector2& Vector2::operator+=(const Vector2& that)
@@ -55,7 +56,18 @@ Vector2& Vector2::operator/=(float scalar)
 
 
 
-Vector2 Vector2::dot(const Vector2& right_side, const Vector2& left_side)
+Vector2 Vector2::getNormalized() const
+{
+	return *this / getLenght();
+}
+
+float Vector2::getLenght() const
+{
+	return std::sqrt(getSquaredLenght());
+}
+
+
+float Vector2::dot(const Vector2& right_side, const Vector2& left_side)
 {
 	return (right_side.x * left_side.x) + (right_side.y * left_side.y);
 }
