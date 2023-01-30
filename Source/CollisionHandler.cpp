9c9c20cd709c0313563d68c5932a708b61a514f7
Martiriak <dominiquepegoraro@gmail.com>
2023-01-30 15:54:44 +0100

    Improved collision handling, by including more precise normals.

--- Source/CollisionHandler.cpp
+++ Source/CollisionHandler.cpp
@@ -6,7 +6,7 @@
 
 namespace
 {
-	bool areIntersecting(const Ball& ball, const Quad& quad)
+	bool areIntersecting(const Ball& ball, const Quad& quad, Vector2& quad_normal_hit)
 	{
 		const float squared_radius = ball.getRadius() * ball.getRadius();
 		const Vector2 quad_to_ball = { std::abs(ball.position.x - quad.position.x), std::abs(ball.position.y - quad.position.y) };
@@ -14,13 +14,44 @@ namespace
 		if (quad_to_ball.x > (quad.getWidth() * 0.5f + ball.getRadius())) return false;
 		if (quad_to_ball.y > (quad.getHeight() * 0.5f + ball.getRadius())) return false;
 
-		if (quad_to_ball.x <= (quad.getWidth() * 0.5f)) return true;
-		if (quad_to_ball.y <= (quad.getHeight() * 0.5f)) return true;
+		if (quad_to_ball.x <= (quad.getWidth() * 0.5f))
+		{
+			if (ball.position.y - quad.position.y > 0.f) quad_normal_hit = Vector2::up();
+			else quad_normal_hit = -Vector2::up();
+
+			return true;
+		}
+		if (quad_to_ball.y <= (quad.getHeight() * 0.5f))
+		{
+			if (ball.position.x - quad.position.x > 0.f) quad_normal_hit = Vector2::right();
+			else quad_normal_hit = -Vector2::right();
+
+			return true;
+		}
 
 		const float corner_distance_squared =
 			Vector2{ quad_to_ball.x - quad.getWidth() * 0.5f, quad_to_ball.y - quad.getHeight() * 0.5f }.getSquaredLenght();
 
-		return corner_distance_squared <= squared_radius;
+		if (corner_distance_squared <= squared_radius)
+		{
+			if (ball.position.x - quad.position.x > 0.f) quad_normal_hit.x = 1.f;
+			else quad_normal_hit.x = -1.f;
+
+			if (ball.position.y - quad.position.y > 0.f) quad_normal_hit.y = 1.f;
+			else quad_normal_hit.y = -1.f;
+
+			quad_normal_hit.normalize();
+
+			return true;
+		}
+
+		return false;
+	}
+
+	inline bool areIntersecting(const Ball& ball, const Quad& quad)
+	{
+		Vector2 dummy_vector{};
+		return areIntersecting(ball, quad, dummy_vector);
 	}
 
 	inline bool isOutOfBounds(const Vector2& grid_pos)
@@ -41,10 +72,11 @@ CollisionHandler::CollisionHandler(Grid::Type& blocks_grid, Ball& ball, Paddle& 
 GameProgress CollisionHandler::checkForCollisions()
 {
 	bool no_remaining_block = true;
+	Vector2 collision_normal;
 
-	if (areIntersecting(*_ball, *_paddle))
+	if (areIntersecting(*_ball, *_paddle, collision_normal))
 	{
-		_ball->handleCollision(Vector2::up());
+		_ball->handleCollision(collision_normal);
 		_paddle->handleCollision(_ball->velocity.getNormalized());
 
 		// Safety measure.
@@ -59,9 +91,9 @@ GameProgress CollisionHandler::checkForCollisions()
 				{
 					no_remaining_block = false;
 
-					if (areIntersecting(*_ball, *block))
+					if (areIntersecting(*_ball, *block, collision_normal))
 					{
-						_ball->handleCollision(-Vector2::up());
+						_ball->handleCollision(collision_normal);
 						block->handleCollision(_ball->velocity.getNormalized());
 
 						block.release();
